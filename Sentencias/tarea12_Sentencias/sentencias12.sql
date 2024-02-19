9e1e3177b151759df82cc976a995dc82c1d7804f
21ricardozamora <zamoraricardo21288@gamil.com>
2024-02-19 17:14:41 +0000

    Se crea la base de datos

---
+++ Sentencias/tarea12_Sentencias/sentencias12.sql
@@ -0,0 +1,103 @@
+Dadas las siguientes tablas : cliente:
+
+    id: entero autoincremental y clave primaria
+    nombre: texto
+    apelllido1: texto
+    apelllido2: texto
+    ciudad: texto
+    categoria: entero
+
+sqlite> create table cliente (
+   ...> id integer primary key autoincrement,
+   ...> nombre text,
+   ...> apellido1 text,
+   ...> apellido2 text,
+   ...> ciudad text,
+   ...> categoria integer
+   ...> );
+
+comercial
+
+    id: entero autoincremental y clave primaria
+    nombre: texto
+    apelllido1: texto
+    apelllido2: texto
+    categoria: real
+
+sqlite> create table comercial (
+   ...> id integer primary key autoincrement,
+   ...> nombre text,
+   ...> apellido1 text,
+   ...> apellido2 text,
+   ...> categoria real
+   ...> );
+pedido
+
+    id: entero autoincremental y clave primaria
+    total: real
+    fecha: texto
+    id_cliente: entero
+    id_comercial: entero
+
+sqlite> create table pedido (
+   ...> id integer primary key autoincrement,
+   ...> total real,
+   ...> fecha text,
+   ...> id_cliente integer references cliente (id),
+   ...> id_comercial integer references comercial (id)
+   ...> );
+
+
+-- INSERTS
+
+INSERT INTO cliente VALUES(1, 'Aarón', 'Rivero', 'Gómez', 'Almería', 100);
+INSERT INTO cliente VALUES(2, 'Adela', 'Salas', 'Díaz', 'Granada', 200);
+INSERT INTO cliente VALUES(3, 'Adolfo', 'Rubio', 'Flores', 'Sevilla', NULL);
+INSERT INTO cliente VALUES(4, 'Adrián', 'Suárez', NULL, 'Jaén', 300);
+INSERT INTO cliente VALUES(5, 'Marcos', 'Loyola', 'Méndez', 'Almería', 200);
+INSERT INTO cliente VALUES(6, 'María', 'Santana', 'Moreno', 'Cádiz', 100);
+INSERT INTO cliente VALUES(7, 'Pilar', 'Ruiz', NULL, 'Sevilla', 300);
+INSERT INTO cliente VALUES(8, 'Pepe', 'Ruiz', 'Santana', 'Huelva', 200);
+INSERT INTO cliente VALUES(9, 'Guillermo', 'López', 'Gómez', 'Granada', 225);
+INSERT INTO cliente VALUES(10, 'Daniel', 'Santana', 'Loyola', 'Sevilla', 125);
+INSERT INTO comercial VALUES(1, 'Daniel', 'Sáez', 'Vega', 0.15);
+INSERT INTO comercial VALUES(2, 'Juan', 'Gómez', 'López', 0.13);
+INSERT INTO comercial VALUES(3, 'Diego','Flores', 'Salas', 0.11);
+INSERT INTO comercial VALUES(4, 'Marta','Herrera', 'Gil', 0.14);
+INSERT INTO comercial VALUES(5, 'Antonio','Carretero', 'Ortega', 0.12);
+INSERT INTO comercial VALUES(6, 'Manuel','Domínguez', 'Hernández', 0.13);
+INSERT INTO comercial VALUES(7, 'Antonio','Vega', 'Hernández', 0.11);
+INSERT INTO comercial VALUES(8, 'Alfredo','Ruiz', 'Flores', 0.05);
+INSERT INTO pedido VALUES(1, 150.5, '2017-10-05', 5, 2);
+INSERT INTO pedido VALUES(2, 270.65, '2016-09-10', 1, 5);
+INSERT INTO pedido VALUES(3, 65.26, '2017-10-05', 2, 1);
+INSERT INTO pedido VALUES(4, 110.5, '2016-08-17', 8, 3);
+INSERT INTO pedido VALUES(5, 948.5, '2017-09-10', 5, 2);
+INSERT INTO pedido VALUES(6, 2400.6, '2016-07-27', 7, 1);
+INSERT INTO pedido VALUES(7, 5760, '2015-09-10', 2, 1);
+INSERT INTO pedido VALUES(8, 1983.43, '2017-10-10', 4, 6);
+INSERT INTO pedido VALUES(9, 2480.4, '2016-10-10', 8, 3);
+INSERT INTO pedido VALUES(10, 250.45, '2015-06-27', 8, 2);
+INSERT INTO pedido VALUES(11, 75.29, '2016-08-17', 3, 7);
+INSERT INTO pedido VALUES(12, 3045.6, '2017-04-25', 2, 1);
+INSERT INTO pedido VALUES(13, 545.75, '2019-01-25', 6, 1);
+INSERT INTO pedido VALUES(14, 145.82, '2017-02-02', 6, 1);
+INSERT INTO pedido VALUES(15, 370.85, '2019-03-11', 1, 5);
+INSERT INTO pedido VALUES(16, 2389.23, '2019-03-11', 1, 5);
+
+
+
+
+    Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han realizado algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los elementos repetidos.
+
+    Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos de los clientes ordenados alfabéticamente.
+
+    Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El resultado debe mostrar todos los datos de los pedidos y de los comerciales. El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
+
+    Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y con los datos de los comerciales asociados a cada pedido.
+
+    Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017, cuya cantidad esté entre 300 € y 1000 €.
+
+    Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún pedido realizado por María Santana Moreno.
+
+    Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
